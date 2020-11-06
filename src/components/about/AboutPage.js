51a29ccb17d626adb070c1c00dcb76ffe105b07f
Agaton Svenaeus <svenaeus.agaton@gmail.com>
2020-11-06 09:12:41 +0100

    Added HomePage and About Page.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutPage() {
+    return (
+    <div>
+        <h2>About</h2>
+        <p>
+            Not much to show at the moment.
+        </p>
+    </div>
+    )
+}
+
+export default AboutPage;
